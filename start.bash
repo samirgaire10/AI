27ed95fc56c39c15f868c2c1ae7d808447c64397
samirgaire10 <samirgaire100@protonmail.com>
2024-11-04 21:58:07 +0900

    run

--- start.bash
+++ start.bash
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Start app.py in the background
 python app.py &
-
-# Start updatemodels.py in the background
 python scripts/updatemodels.py &
 
-# Start the PHP server
 php -S localhost:8000 &
+sleep 3
 
-# Wait for a few seconds
-sleep 5
-
-# Open the default web browser
 xdg-open http://localhost:8000  # For Linux
 # open http://localhost:8000      # For macOS
 # start http://localhost:8000      # For Windows (in WSL)
--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo starting service 
-timeout /t 5 /nobreak > NUL
+timeout /t 3 /nobreak > NUL
 
 start cmd /c "app.py"
 start cmd /c "scripts\updatemodels.py"
 start php -S localhost:8000
-timeout /t 5 /nobreak > NUL
+timeout /t 3 /nobreak > NUL
 start http://localhost:8000
